74514468bc32d6252c247d76f4bc851b9fe4ab0e
jmtek <jing226@gmail.com>
2023-05-10 14:01:13 +0800

    make it works

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+import datetime
 import os
 from tempfile import NamedTemporaryFile
 
@@ -23,7 +24,6 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 init_logger()
-model = whisper.load_model("base")
 logger = logging.getLogger(__name__)
 supported_file_types = ["mp3", "wav", "m4a", "webm", "mpga", "mpeg"]
 audio_file = st.file_uploader("选择一个音频文件:", type=supported_file_types)
@@ -34,13 +34,17 @@ def load_model():
     model = whisper.load_model("small")
     return model
 
+def format_time(num_str):
+    return str(datetime.timedelta(seconds=int(num_str)))
 
 def transcribe(audio_file):
-    # with NamedTemporaryFile() as temp:
-    #     with st.spinner("请耐心等待 ..."):
+    with NamedTemporaryFile() as temp:
+        with st.spinner("请耐心等待 ..."):
 
-    #         temp.write(audio_file.getvalue())
-    #         temp.seek(0)
+            temp.write(audio_file.getvalue())
+            temp.seek(0)
+
+            # return { "text": temp.name }
 
     #         audio_file = temp.name
     #         try:
@@ -49,28 +53,36 @@ def transcribe(audio_file):
     #             logger.error("The file could not be transcribed, file: " + temp.name)
     #     return result
 
+            model = load_model()
 
-    audio = whisper.load_audio(audio_file.filename)
-    audio = whisper.pad_or_trim(audio)
+            audio = whisper.load_audio(temp.name)
+            audio = whisper.pad_or_trim(audio)
 
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+            # fp16 should be False if use CPU as it is not supported on CPU
+            return model.transcribe(audio, verbose=True, fp16 = False)
 
-    _, probs = model.detect_language(mel)
+            mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    options = whisper.DecodingOptions(fp16 = False)
-    result = whisper.decode(model, mel, options)
+            _, probs = model.detect_language(mel)
 
-    return result
+            options = whisper.DecodingOptions(fp16 = False)
+            result = whisper.decode(model, mel, options)
+
+            return result
         
 
 if audio_file is not None:
     logger.debug("transcript started...")
-    st.write(str(audio_file))
     result = transcribe(audio_file)
 
-    if result["text"] == "":
-        output = "Speech was not detected"
-    else:
-        output = result["text"]
-    logger.debug("transcript is: " + output)
-    st.write(output)
+    # if result["text"] == "":
+    #     output = "Speech was not detected"
+    # else:
+    #     output = result["text"]
+    logger.debug("transcript is: " + str(result["text"]))
+
+    st.text_area(label='Text & Timeline', value='\n'.join([ f"[{format_time(segment['start'])} --> {format_time(segment['end'])}]  {segment['text']}" for segment in result["segments"]]), height=300)
+
+    st.text_area(label='Only Text', value='\n'.join([ f"{segment['text']}" for segment in result["segments"]]), height=300)
+    
+    # st.text_area(label="Raw Obj", value=str(result), height=300)
